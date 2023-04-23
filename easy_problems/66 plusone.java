2a8d03f06cdc1c8e33eeec70f00300b99e213be1
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-04-23 14:01:05 +0530

    Create 66 plusone.java

---
+++ easy_problems/66 plusone.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        for(int i=len-1; i>=0; i--){
+            if(digits[i] != 9){
+                digits[i]+=1;
+                return digits;
+            }else{
+                digits[i] = 0;
+            }
+        }
+        int[] arr = new int[len+1];
+        arr[0] = 1;
+        
+        return arr;
+    }
+}
