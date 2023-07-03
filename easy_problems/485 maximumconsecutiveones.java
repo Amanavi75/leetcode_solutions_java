eb92198e66714c4ad38dd5bec963deb7ab75112b
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-07-04 00:01:07 +0530

    Create 485 maximumconsecutiveones.java

---
+++ easy_problems/485 maximumconsecutiveones.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxi=0;
+        int count =0;
+        for(int i =0;i<nums.length;i++){
+            if(nums[i]==1){
+                count++;
+                maxi=Math.max(maxi,count);
+            }else{
+                count =0;
+            }
+        }
+         return maxi;
+    }
+   
+}
