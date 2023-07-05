096eb44fe9d6b90c5a1f30bfd578e791210d0cd0
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-07-05 09:57:13 +0530

    Update 121 Best Time to Buy and Sell Stock.java

--- easy_problems/121 Best Time to Buy and Sell Stock.java
+++ easy_problems/121 Best Time to Buy and Sell Stock.java
@@ -1,14 +1,19 @@
+
+// we will buy  on that day when the price is minimum from 1st to i-1th day and after that we will compare with the all day and check on which day profit is maximum 
+
+ 
+
+
 class Solution {
     public int maxProfit(int[] prices) {
         
-    int sellOne = 0;
-    int holdOne = Integer.MIN_VALUE;
-
-    for (final int price : prices) {
-      sellOne = Math.max(sellOne, holdOne + price);
-      holdOne = Math.max(holdOne, -price);
-    }
-
-    return sellOne;
+   int mini =prices[0];
+   int maxProfit = 0;
+   for (int i =0;i<prices.length;i++){
+       int cost = prices[i]-mini;
+       maxProfit= Math.max(maxProfit,cost);
+       mini = Math.min(mini,prices[i]);
+   }
+     return maxProfit;
     }
 }
