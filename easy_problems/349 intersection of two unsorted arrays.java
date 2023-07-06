bb84e4f6c7c7e6e583e2c46e8b0ccab0a0cf0ba0
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-07-06 11:12:53 +0530

    Update 349 intersection of two unsorted arrays.java

--- easy_problems/349 intersection of two unsorted arrays.java
+++ easy_problems/349 intersection of two unsorted arrays.java
@@ -1,12 +1,27 @@
+//Use two hashsets and input the elements of first array in the first hashset and remove duplicates while input of the second array elements.
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-      List<Integer> ans = new ArrayList<>();
-    Set<Integer> set = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
+        HashSet<Integer> n1 = new HashSet<>();
+        HashSet<Integer> n2 = new HashSet<>();
+        
+        for(int i=0;i<nums1.length;i++){
+            n1.add(nums1[i]);
+        }
+        
+        for(int i=0;i<nums2.length;i++){
+            if(n1.contains(nums2[i])){
+                n2.add(nums2[i]);
+            }
+        }
 
-    for (final int num : nums2)
-      if (set.remove(num))
-        ans.add(num);
+        int[] arr = new int[n2.size()];
 
-    return ans.stream().mapToInt(Integer::intValue).toArray();
+        int k =0;
+        for(int i: n2){
+            arr[k] = i;
+            k++;
+        }
+
+        return arr;
     }
 }
