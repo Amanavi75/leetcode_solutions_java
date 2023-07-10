c790081eda8dfe6f66fb29193aac9388c34543ba
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-07-10 23:01:51 +0530

    Update 20 valid parenthesis.java

--- easy_problems/20 valid parenthesis.java
+++ easy_problems/20 valid parenthesis.java
@@ -1,23 +1,26 @@
+// create an empty stack
+// loop through each character in the string
+//if the character is an opening parenthesis ush the corresponding closing parenthesis onto the stack and smae with all the remaining parenthesis
+//if the character is a closing bracket
+// if the stack is empty (i.e., there is no matching opening bracket) or the top of the stack
+// does not match the closing bracket, the string is not valid, so return false
+
+
+
 class Solution {
     public boolean isValid(String s) {
-      Stack<Character>leftSymbols= new Stack();
-      for(char c : s.toCharArray()){
-          if (c =='('|| c=='{'||c=='['){
-              leftSymbols.push(c);
-          }
-          else if(c==')' && !leftSymbols.isEmpty() && leftSymbols.peek()=='('){
-              leftSymbols.pop();
-          }
-          else if(c=='}' && !leftSymbols.isEmpty() && leftSymbols.peek()=='{'){
-              leftSymbols.pop();
-          }
-          else if(c==']' && !leftSymbols.isEmpty() && leftSymbols.peek()=='['){
-              leftSymbols.pop();
-          }
-          else {
-              return false ;
-          }
-      }   
-      return leftSymbols.isEmpty();
+     Stack<Character> stack = new Stack<Character>(); 
+        for (char c : s.toCharArray()) { 
+            if (c == '(') {
+                stack.push(')'); 
+            }else if(c=='{')
+                stack.push('}'); 
+            
+            else if (c == '[') 
+                stack.push(']'); 
+            else if (stack.isEmpty() || stack.pop() != c) 
+                return false;
+        }
+        return stack.isEmpty();
     }
 }
