26690179e0178ff063b6984efae9a677fe10cf20
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-07-16 08:43:15 +0530

    Update 485 maximumconsecutiveones.java

--- easy_problems/485 maximumconsecutiveones.java
+++ easy_problems/485 maximumconsecutiveones.java
@@ -1,3 +1,23 @@
+//We maintain a variable count that keeps a track of the number of consecutive 1’s while traversing the array. 
+//The other variable max_count maintains the maximum number of 1’s, 
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxi=0;
+        int count =0;
+        for(int i =0;i<nums.length;i++){
+            if(nums[i]==1){
+                count++;
+                maxi=Math.max(maxi,count);
+            }else{
+                count =0;
+            }
+        }
+         return maxi;
+    }
+   
+}
+
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         int maxi=0;
