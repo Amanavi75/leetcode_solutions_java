b0120da45f0cc189d1907bde937b69fb2defc993
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-07-16 12:48:31 +0530

    Update 169 majorityelement.java

--- easy_problems/169 majorityelement.java
+++ easy_problems/169 majorityelement.java
@@ -1,21 +1,26 @@
 class Solution {
     public int majorityElement(int[] nums) {
-       int majorityCount = nums.length/2;
+        int n = nums.length;
+        int cnt = 0; // count
+        int el = 0; // Element
 
-        for (int num : nums) {
-            int count = 0;
-            for (int elem : nums) {
-                if (elem == num) {
-                    count += 1;
-                }
-            }
-
-            if (count > majorityCount) {
-                return num;
-            }
-
+        //applying the algorithm:
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = nums[i];
+            } else if (el == nums[i]) cnt++;
+            else cnt--;
         }
 
-        return -1;      
+        //checking if the stored element
+        // is the majority element:
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el) cnt1++;
+        }
+
+        if (cnt1 > (n / 2)) return el;
+        return -1;
     }
 }
