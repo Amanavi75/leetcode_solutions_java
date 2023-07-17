0c0b9a6fce8873c0d159037068735acd6a409b69
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-07-17 14:17:48 +0530

    Update 53 Maximum Subarray.java

--- medium_problems/53 Maximum Subarray.java
+++ medium_problems/53 Maximum Subarray.java
@@ -1,13 +1,22 @@
+//We will run a loop(say i) to iterate the given array.
+//Now, while iterating we will add the elements to the sum variable and consider the maximum one.
+//f at any point the sum becomes negative we will set the sum to 0 as we are not going to consider it as a part of our answer.
+
+
 class Solution {
     public int maxSubArray(int[] nums) {
-     int ans = Integer.MIN_VALUE;
-    int sum = 0;
+     int max = nums[0];
+     int sum = 0;
 
-    for (final int num : nums) {
-      sum = Math.max(num, sum + num);
-      ans = Math.max(ans, sum);
-    }
-
-    return ans;
+    for(int i =0;i<nums.length;i++){
+        sum+=nums[i];
+        if(sum>max){
+            max=sum;
+        }
+        if(sum<0){
+            sum=0;
+        }
+       }
+       return max;
     }
 }
