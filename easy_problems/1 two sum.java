ecf12751cc6bf30cd3e6d83de4bcc012611ce2f9
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-09-12 23:29:01 +0530

    Update 1 two sum.java

--- easy_problems/1 two sum.java
+++ easy_problems/1 two sum.java
@@ -1,4 +1,4 @@
-// we will use two pointer approach in which we will first sort the element and after we will put first pointer at first index and last pointer at last element  if we have to  increment we will move our first pointer ahead if we have to decrease then move last pointer one step back  
+// approach - we will use two pointer approach in which we will first sort the element and after we will put first pointer at first index and last pointer at last element  if we have to  increment we will move our first pointer ahead if we have to decrease then move last pointer one step back  
 
 // we will use the HashMap to check if the other element i.e. target-(selected element) exists. Thus we can trim down the time complexity of the problem.
 //And for the second variant, we will store the element along will its index in the HashMap. Thus we can easily retrieve the index of the other element i.e. target-(selected element) without iterating the array. 
