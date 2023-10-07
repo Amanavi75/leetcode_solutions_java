1bb990ed4ea605b6ce70ebb769bdfb73bd8a27b4
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-07 11:46:00 +0530

    Create 543 diameter of a  binary tree.java

---
+++ easy_problems/543 diameter of a  binary tree.java
@@ -0,0 +1,22 @@
+/**
+  for every node observe the left height and right height 
+  and add them up ,and find the max of that ultimately we will be able get
+  the diameter of the binary tree   
+   */
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+
+       int[] diameter = new int[1];
+       height(root,diameter);
+       return diameter[0]; 
+    }
+    private int height(TreeNode node , int[] diameter){
+        if(node==null){
+            return 0;
+        }
+        int lh = height(node.left,diameter);
+        int rh = height(node.right,diameter);
+        diameter[0]= Math.max(diameter[0],lh+rh);
+        return 1 +Math.max(lh,rh);
+    }
+}
