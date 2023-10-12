0f200230410aa7463f9a8c0f2015c13c8375cb21
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-12 19:10:26 +0530

    Update 101 symmetric tree.java

--- easy_problems/101 symmetric tree.java
+++ easy_problems/101 symmetric tree.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-      return isSymmetric(root, root);
-  }
-
-  private boolean isSymmetric(TreeNode p, TreeNode q) {
-    if (p == null || q == null)
-      return p == q;
-
-    return p.val == q.val && isSymmetric(p.left, q.right) && isSymmetric(p.right, q.left);  
+        return root==null || isSymmetricHelp(root.left,root.right);
+    }
+    private boolean isSymmetricHelp(TreeNode left, TreeNode right){
+        if(left==null ||right==null){
+            return left==right;
+        }
+        if(left.val!=right.val){
+            return false;
+        }
+        return isSymmetricHelp(left.left,right.right) && isSymmetricHelp(left.right,right.left);
     }
 }
