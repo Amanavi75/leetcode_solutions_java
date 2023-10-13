699c28a1e073eb625384774dd39ec80cac659f55
Aman avi <73480406+Amanavi75@users.noreply.github.com>
2023-10-13 23:00:28 +0530

    Update 746 min cost climbing stairs.java

--- easy_problems/746 min cost climbing stairs.java
+++ easy_problems/746 min cost climbing stairs.java
@@ -1,12 +1,14 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length;
-	    return Math.min(minCost(cost, n-1), minCost(cost, n-2));
+    int n = cost.length;
+    int first = cost[0];
+    int second = cost[1];
+    if (n <= 2) return Math.min(first, second);
+    for (int i = 2; i < n; i++) {
+        int curr = cost[i] + Math.min(first, second);
+        first = second;
+        second = curr;
     }
-    private int minCost(int[] cost, int n) {
-	    if (n < 0) return 0;
-	    if (n==0 || n==1) return cost[n];
-	    return cost[n] + Math.min(minCost(cost, n-1), minCost(cost, n-2));
-        
+    return Math.min(first, second);
     }
 }
